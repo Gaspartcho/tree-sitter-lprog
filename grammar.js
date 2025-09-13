123d55a7007e6b1a076f2dd3376a3da4f7288806
Gaspard Deremble <g.deremble@laposte.net>
2025-09-13 21:09:00 +0200

    added comments again

--- grammar.js
+++ grammar.js
@@ -56,7 +56,7 @@ module.exports = grammar({
 
     // Taken from the c grammar
     // http://stackoverflow.com/questions/13014947/regex-to-match-a-c-style-multiline-comment/36328890#36328890
-    comment: $ => /%[^%]%/,
+    comment: $ => seq("%", repeat(/[^%]/), "%"),
 
     variable: $ => /[a-z_]+/,
     macro: $ => /[A-Z_]+/,
