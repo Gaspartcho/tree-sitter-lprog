1bef209d06775e4e1958177eae340809fa172e57
Gaspard Deremble <g.deremble@laposte.net>
2025-10-21 09:00:42 +0200

    more nice parenthesis logic

--- grammar.js
+++ grammar.js
@@ -10,64 +10,51 @@
 module.exports = grammar({
   name: "lprog",
 
-
-  extras: $ => [
-    /\s|\\\r?\n/,
-    $.comment
-  ],
+  extras: ($) => [/\s|\\\r?\n/, $.comment],
 
   rules: {
     // TODO: add the actual grammar rules
-    source_file: $ => repeat($._expression),
+    source_file: ($) => repeat($._expression),
 
-
-
-    _expression: $ => choice(
-      $.function,
-      $.application,
-      $.variable,
-      $.macro,
-      $.directive,
-      $.string,
-      $.dire_loop
-    ),
-
-    function: $ => seq(
-      "\\",
-      seq(
-        repeat(seq(
-          field("variable", $.variable),
-          optional(",")
-        )),
-        field("variable", $.variable)
+    _expression: ($) =>
+      choice(
+        $.function,
+        $.application,
+        $.variable,
+        $.macro,
+        $.directive,
+        $.string,
+        $.dire_loop,
       ),
-      ".",
-      field("body", $._expression)
-    ),
 
-    application: $ => seq(
-      "(",
-      field("function", $._expression),
-      repeat1(seq(
-        optional(","),
-        field("arg", $._expression)
-      )),
-      ")"
-    ),
+    function: ($) =>
+      seq(
+        "\\",
+        seq(
+          repeat(seq(field("variable", $.variable), optional(","))),
+          field("variable", $.variable),
+        ),
+        ".",
+        field("body", $._expression),
+      ),
 
-    dire_loop: $ => seq(
-      "[",
-      field("body", $.directive),
-      "]"
-    ),
+    application: ($) =>
+      seq(
+        "(",
+        field("function", $._expression),
+        repeat(seq(optional(","), field("arg", $._expression))),
+        ")",
+      ),
+
+    dire_loop: ($) => seq("[", field("body", $.directive), "]"),
 
     // Taken from the c grammar
     // http://stackoverflow.com/questions/13014947/regex-to-match-a-c-style-multiline-comment/36328890#36328890
-    comment: $ => seq("%", repeat(/[^%]/), "%"),
+    comment: ($) => seq("%", repeat(/[^%]/), "%"),
 
-    variable: $ => /[a-z_]+/,
-    macro: $ => /[A-Z_]+/,
-    string: $ => /"[^"]*"/,
-    directive: $ => /[#$&@\*!?\+\-]+/
-  }
+    variable: ($) => /[a-z_]+/,
+    macro: ($) => /[A-Z_]+/,
+    string: ($) => /"[^"]*"/,
+    directive: ($) => /[#$&@\*!?\+\-]+/,
+  },
 });
